497af08bdeb9d41bf12801a01e6032228f470a56
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-07-28 12:01:42 -0400

    Initial commit.

---
+++ DocumentInteraction/DocumentInteraction/AppDelegate.m
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.m
+//  DocumentInteraction
+//
+//  Created by Jeffrey Kereakoglow on 7/27/15.
+//  Copyright (c) 2015 Alexis Digital. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+@end
---
+++ DocumentInteraction/DocumentInteraction/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  DocumentInteraction
+//
+//  Created by Jeffrey Kereakoglow on 7/27/15.
+//  Copyright (c) 2015 Alexis Digital. All rights reserved.
+//
+
+@import UIKit;
+@import QuickLook;
+
+@interface MasterViewController : UITableViewController <QLPreviewControllerDataSource, QLPreviewControllerDelegate, UIDocumentInteractionControllerDelegate>
+
+@end
+
