c9b6c4be782e0966f75bf504c0bffdfb049cb185
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-07-28 14:22:50 -0400

    Indent code

--- DocumentInteraction/DocumentInteraction/ADTableViewModel.m
+++ DocumentInteraction/DocumentInteraction/ADTableViewModel.m
@@ -80,7 +80,9 @@
 
 #pragma mark - Helpers
 /**
- Searches the main bundle for a property list with the name provided by the argument @c propertyListName. The root item of the property list must be a dictionary because this method returns an array of dictionary keys.
+ Searches the main bundle for a property list with the name provided by the
+ argument @c propertyListName. The root item of the property list must be a
+ dictionary because this method returns an array of dictionary keys.
  @param propertyListName The name of the property list to load.
  @since 1.0
  @author Jeff Kereakoglow
@@ -89,14 +91,18 @@
   NSString* resourcePath = [[NSBundle mainBundle] pathForResource:propertyListName
                                                            ofType:@"plist"];
 
-  NSAssert(resourcePath, @"\n\n  ERROR in %s: Could not find the property list resource.\n\n", __PRETTY_FUNCTION__);
+  NSAssert(resourcePath,
+           @"\n\n  ERROR in %s: Could not find the property list resource.\n\n",
+           __PRETTY_FUNCTION__);
 
   NSArray *items;
 
   items = [NSArray arrayWithContentsOfFile:resourcePath];
 
-  NSAssert(items, @"\n\n  ERROR in %s: Could not load the property list resource. Make sure the root item of the property list is a Dictionary.\n\n", __PRETTY_FUNCTION__);
-
+  NSAssert(items,
+           @"\n\n  ERROR in %s: Could not load the property list resource. Make sure the root item of the property list is a Dictionary.\n\n",
+           __PRETTY_FUNCTION__);
+  
   return items;
 }
 
