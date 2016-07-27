0dd1779f0267de35473005367c7e97d74fe2eef0
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-07-26 20:54:27 -0400

    Reorganize files to reflect Xcode groups

---
+++ Source/ADTableViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  DocumentInteraction
+//
+//  Created by Jeffrey Kereakoglow on 7/27/15.
+//  Copyright (c) 2015 Alexis Digital. All rights reserved.
+//
+
+@import UIKit;
+@import QuickLook;
+
+/**
+ The responsibility of this class is to handle actions initiated by the user.
+ */
+@interface ADTableViewController: UITableViewController <UITableViewDelegate, QLPreviewControllerDelegate>
+
+@end
+
---
+++ Source/AppDelegate.m
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.m
+//  DocumentInteraction
+//
+//  Created by Jeffrey Kereakoglow on 7/27/15.
+//  Copyright (c) 2015 Alexis Digital. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+@end
