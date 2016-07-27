f83e64e0d82cea6236915567e44b90ebad41ba1d
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-07-26 21:02:37 -0400

    Minor clean up

--- SupportFiles/main.m
+++ SupportFiles/main.m
@@ -6,11 +6,11 @@
 //  Copyright (c) 2015 Alexis Digital. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
+int main(int argc, char *argv[]) {
+  @autoreleasepool {
+    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
 }
